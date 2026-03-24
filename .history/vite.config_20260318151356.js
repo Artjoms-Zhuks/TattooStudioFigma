3eda60d14ff2c3ed8d50fb369020985f9a5008d7
Artjoms Zuks <artjoms.zuks@gmail.com>
2026-03-24 12:29:10 +0200

    fix base path

--- .history/vite.config_20260318151356.js
+++ .history/vite.config_20260318151356.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Добавьте эту строку точно как здесь
+  base: '/TattooStudioFigma/', // Добавьте эту строку точно как здесь
 })
